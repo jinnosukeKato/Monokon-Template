4abd7ed7c43a0db6c9d406cb199d701276904e4d
jinnosukeKato <96569057+jinnosukeKato@users.noreply.github.com>
2022-07-21 14:01:36 +0900

    いろいろ追加

--- monokon_template.ino
+++ monokon_template.ino
@@ -1,3 +1,5 @@
+#define CLOCK_MS 10
+
 const int SEGMENT[11][8] = {
   {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW, LOW}, //0
   {LOW, HIGH, HIGH, LOW, LOW, LOW, LOW, LOW}, //1
@@ -12,22 +14,30 @@ const int SEGMENT[11][8] = {
   {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW} //OFF
 };
 
+// アウトプットピン
 const int OUT_PIN[] = {2, 4, 5, 12, 13, 14, 15, 16, 17, 18, 19};
+
+// インプットプットピン
 const int IN_PIN[] = {22, 24, 26};
 
 void setup() {
+  // アウトプットピンの初期化
   for (int i : OUT_PIN) {
     pinMode(i, OUTPUT);
   }
+  
+  // インプットピンの初期化
   for (int i : IN_PIN) {
     pinMode(i, INPUT);
   }
+  // 一度だけ実行するコードはこの下に
 }
 
 void loop() {
-  
+  // 繰り返し実行するコードはこの下に
 }
 
+// セグメント両側点灯しつつステッピングを回す関数
 void segStep(int l, int r) {
   for (int n = 0; n < 4; n++) {
     digitalWrite(2, LOW);
@@ -42,12 +52,21 @@ void segStep(int l, int r) {
   }
 }
 
-void stepping() {
+// ステッピング右回転
+void steppingR() {
+  for (int n = 3; n >= 0; n--) {
+    _step(n);
+  }
+}
+
+// ステッピング左回転
+void steppingL() {
   for (int n = 0; n < 4; n++) {
     _step(n);
   }
 }
 
+// ステッピング1/4ステップ
 void _step(int n) {
   int ptn[4][4] = {
     {HIGH, HIGH, LOW, LOW},
@@ -62,12 +81,14 @@ void _step(int n) {
   clk();
 }
 
+// クロック
 void clk() {
   digitalWrite(5, HIGH);
-  delay(10);
+  delay(CLOCK_MS);
   digitalWrite(5, LOW);
 }
 
+// セグメント両点灯
 void segW(int l, int r) {
   segL(l);
   delay(10);
@@ -75,18 +96,21 @@ void segW(int l, int r) {
   delay(10);
 }
 
+// 右セグメント点灯
 void segR(int num) {
   digitalWrite(2, LOW);
   digitalWrite(3, HIGH);
   seg(num);
 }
 
+// 左セグメント点灯
 void segL(int num) {
   digitalWrite(2, HIGH);
   digitalWrite(3, LOW);
   seg(num);
 }
 
+// セグメントにデータ送信
 void seg(int num) {
   for (int i = 12; i <= 19; i++) {
     digitalWrite(i, SEGMENT[num][i - 12]);
